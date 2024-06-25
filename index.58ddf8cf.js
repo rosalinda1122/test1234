9680c6e35f90f99777fca1b1095d866b8e65f340
rosalinda1122 <160768987+rosalinda1122@users.noreply.github.com>
2024-06-25 17:30:45 +0000

    Deploying to gh-pages from @ rosalinda1122/test1234@a70c53433b9d9ee64b2ceb670e6b21306a4abb6f 🚀

--- index.58ddf8cf.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let o=!1;function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,setTimeout(n,1e3)}t.addEventListener("click",(()=>{o=!0,n()})),e.addEventListener("click",(()=>{}));
-//# sourceMappingURL=index.58ddf8cf.js.map
