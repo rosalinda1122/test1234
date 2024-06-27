1e6248e850e6a5ac06f7504fd4c02ec8b39d349b
Różowa <roserutkowska@gmail.com>
2024-06-27 20:00:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-function getRandomHexColor() {  
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    }
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
-let isChangingColor = false;
-
-function changeBackgroundColor() {
-    document.body.style.backgroundColor = getRandomHexColor();
-    const timer = setInterval(changeBackgroundColor, 1000);
-}
+const body = document.querySelector('body');
+let timeoutId;
 startButton.addEventListener('click', () => {
-    isChangingColor = true;
-    changeBackgroundColor ()
+  // Wyłącz przycisk "Start"
+  startButton.disabled = true;
+  // Rozpocznij zmianę koloru tła
+  changeBackgroundColor();
 });
+stopButton.addEventListener('click', () => {
+  // Wyczyść timeout, aby zatrzymać zmianę koloru
+  clearTimeout(timeoutId);
+  // Włącz przycisk "Start"
+  startButton.disabled = false;
+});
+function changeBackgroundColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  timeoutId = setTimeout(changeBackgroundColor, 1000);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-stopButton.addEventListener('click', () =>{ 
-    function stopColor() {
-        clearInterval(timer);
-}});
+ 
+
